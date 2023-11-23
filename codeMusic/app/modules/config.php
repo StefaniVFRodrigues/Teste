d06ed659154731ccff99672eb4e65c1800d7cb4d
StefaniVFRodrigues <tefarodritas@gmail.com>
2023-11-23 15:16:58 -0400

    config da database acertada e add arquivo .htacess

--- codeMusic/app/modules/config.php
+++ codeMusic/app/modules/config.php
@@ -1,12 +1,12 @@
 <?php
 /* Configurações do banco de dados */
-    define('DB_NAME', 'spotify');
-    define('DB_USER', 'root');
-    define('DB_PASS', 'root');
-    define('DB_HOST', 'localhost:8889');
+    define('DB_NAME', 'codemusic');
+    define('DB_USER', 'codemusic');
+    define('DB_PASS', 'code3202P@ss');
+    define('DB_HOST', '47.254.124.113');
 
-    define('URL_BASE', 'http://localhost:8888/codeMusic');
+    define('URL_BASE', 'http://localhost/projeto/codeMusic');
     define('URL_PROFILE_PIC', URL_BASE.'/src/uploads/profilePicture');
     define('URL_COVER_PIC', URL_BASE.'/src/uploads/coverFile');
 
-    define('DIR_BASE', '/Applications/MAMP/htdocs/codeMusic');
+    define('DIR_BASE', 'C:\xampp\htdocs\projeto\codeMusic');
